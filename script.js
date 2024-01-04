5cd280f1594246cca0dd668253f3f983104dc7a6
phamghien <phamghien@gmail.com>
2024-01-04 15:53:49 -0500

    Fix grid. Change square color to black when mouse hovers the square.

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+const DEFAULT_SIZE = 16;
+
+const grid = document.getElementById("grid");
+const square = document.querySelectorAll(".square"); 
 const colorButton = document.querySelector(".color"); 
 const sizeButton = document.querySelector(".size"); 
 const eraseButton = document.querySelector(".erase"); 
 
-eraseButton.addEventListener("click", function(){
-    for (let i = 0; i < val*val; i++) {
-        cell[i].style.backgroundColor = '#d4d4d4';
-    }
-}); 
+// Create grid 
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-createGrid = () => {
-    for (let i = 0; i < 256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('cell');
-        div.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'black';
-        })
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
         grid.appendChild(div); 
+        div.classList.add("square");
+
+        // Change the color to black when mouse goes over square
+        div.addEventListener("mouseover", function trace(e) {
+            div.style.backgroundColor = "#000000";
+        });
     }
-    };
+}
 
-sizeButton.addEventListener("click", changeSize); 
-colorButton.addEventListener("click", changeColor); 
-
-
+// Once DOM is loaded, create grid 
+document.addEventListener("DOMContentLoaded", function () {
+   createGrid(DEFAULT_SIZE); 
+});
